81b9242a1532b31d59719798769c95c344b843da
LishadWahid <lishadwahid@gmail.com>
2025-11-13 20:51:38 +0300

    last update

--- src/components/firebase/firebase.init.js
+++ src/components/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAHEWsnIxkxnsNilJEzJ_1uJ-sRNDvmy7w",
-  authDomain: "pawmart-server-client.firebaseapp.com",
-  projectId: "pawmart-server-client",
-  storageBucket: "pawmart-server-client.firebasestorage.app",
-  messagingSenderId: "27810600739",
-  appId: "1:27810600739:web:09c8afa1b76029081d9e96"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
